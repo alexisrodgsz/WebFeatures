35ed3ed925b3d9e565fb1a3f23b338396d6fbe9e
Alexis Rodriguez <axisrod09@gmail.com>
2021-02-27 12:53:07 -0500

    added section fc

---
+++ BarSteps/script.js
@@ -0,0 +1,75 @@
+const progess = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+let currentActive = 1
+
+
+next.addEventListener('click', () => {
+
+    currentActive++;
+
+    /* console.log(cuurentActive);*/
+
+    if(currentActive > circles.length){
+
+        currentActive = circles.length;
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+
+    currentActive--;
+
+    /* console.log(cuurentActive);*/
+
+    if(currentActive < 1){
+
+        currentActive = 1;
+    }
+
+    update()
+})
+
+
+function update(){
+
+    circles.forEach( (circle, idx) =>{
+
+        if(idx <  currentActive){
+
+            circle.classList.add('active')
+        } else {
+
+            circle.classList.remove('active')
+        }
+
+
+    })
+
+    const actives  = document.querySelectorAll('.active');
+
+    progress.style.width = (actives.length-1) / (circles.length-1 ) * 100 + '%';
+    
+    console.log(actives.length);
+    console.log(circles.length);
+
+
+    console.log((actives.length-1) / (circles.length-1 ) * 100 + '%');
+
+
+    if(currentActive == 1){
+
+        prev.disabled = true;
+    }else if(currentActive == circles.length){
+
+        next.disabled = true;
+
+    }else {
+
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
